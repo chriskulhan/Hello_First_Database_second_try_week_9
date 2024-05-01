97b4f1e629b1848c64dfd8269f70cca05676ec8e
chriskulhan <156239549+chriskulhan@users.noreply.github.com>
2024-05-01 12:43:04 -0500

    video 7 or 1b (second set of vids for week 9, pause 10:12

--- src/main/java/Movies_vid_1b/Database.java
+++ src/main/java/Movies_vid_1b/Database.java
@@ -4,6 +4,7 @@ package Movies_vid_1b;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Database {
@@ -15,12 +16,28 @@ public class Database {
     //need to know where the movie table is stored
         //-pass that in the argument area () after Database vv:
     Database(String databasePath){
+        //3. add try catch with resources:
+
         //create table, or make sure it is created:
         //connect and create a table:
         //connection is the connection to the database (on the computer or on the internet)
-        Connection connection = DriverManager.getConnection(databasePath);
+       try ( Connection connection = DriverManager.getConnection(databasePath);
 
         //statement can execute SQL statements (execute, create table, etc.)
-        Statement statement = connection.createStatement();
+        Statement statement = connection.createStatement()) {
+
+           //4. ask the statement to do our SQL for us:
+           statement.executeUpdate("CREATE TABLE IF NOT EXISTS " +
+                   "movies(name text, stars integer, watch boolean)");
+
+
+       } catch (SQLException e) {
+           System.out.println("Error creating movie DB table because " + e);
+       }
+    }
+    //5. write a method: void, called addNewMovie with Movie objects that will store data
+    //and move them around (Movie movie)
+    public void addNewMovie(Movie movie) {
+        //todo (mine) be sure to come back and finish this.
     }
 }
